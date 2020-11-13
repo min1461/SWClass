31db9eadb2d131040eed6cde0b2f3b25b436b22d
min1461 <min1461@gmail.com>
2020-11-13 16:37:10 +0900

    startWith / endWith 시작과 끝 문자열찾기

---
+++ SWStudy/src/Strings/startsWith.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class startsWith {
+	public static void main(String[] args) {
+		String s1 = "good";
+		if(s1.startsWith("go")) {	//매개변수로 시작하면~
+			System.out.println(s1);
+		}
+		if(s1.endsWith("ood")) {	//매개변수로 끝나면~
+			System.out.println(s1);
+		}
+	}
+}
