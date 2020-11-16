bc4ce8d126eb891860c860d54c533afb4fa9a39f
min1461 <min1461@naver.com>
2020-11-16 21:48:56 +0900

    generic, Linkedlist, HashMap

--- SWStudy/src/Arrays/generic.java
+++ SWStudy/src/Arrays/generic.java
@@ -1,4 +1,7 @@
 package Arrays;
+
+import java.util.ArrayList;
+
 /*
  * 배열과 컬렉션의 장점을 따서 만들어보자
  * 가변길이배열 + one data type
@@ -10,5 +13,25 @@ package Arrays;
  * ArrayList <SMarket> arr3 = new ArrayList<String>();
  */
 public class generic {
+	public static void main(String[] args) {
+		ArrayList <String> arr2 = new ArrayList<String>();
+		System.out.println(arr2.size());
+		arr2.add("빵");
+		arr2.add("라면");
+		arr2.add("우유");
+		arr2.add("김밥");
+//		수정은 set
+//		삭제는 remove
+		
+		arr2.set(1, "과자");
+		arr2.remove(2);
+		arr2.remove("빵");
+		
+		System.out.println(arr2);
+		for(String tmp:arr2) {
+			//for문 초기부터 배열의 끝까지 갈때까지 반복
+			System.out.println(tmp);
+		}
+	}
 	
 }
