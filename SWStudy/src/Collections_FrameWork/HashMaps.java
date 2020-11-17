e3715669a72f63bc0a0c1ba77d3550fcd8663e66
min1461 <min1461@naver.com>
2020-11-18 01:13:40 +0900

    Collections FrameWork

---
+++ SWStudy/src/Collections_FrameWork/HashMaps.java
@@ -0,0 +1,58 @@
+package Collections_FrameWork;
+/*
+ * Iterater Interface
+ */
+import java.util.Collection;
+/*
+ * HashMap
+ *  - Map 인터페이스를 구현하기 위해 해시테이블을 사용한 클래스
+ *  - 중복X, 순서X
+ *  - 키와 값으로 null이 허용
+ */
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMaps {
+	public static void main(String[] args) {
+		HashMap<Integer, String> hm1 = new HashMap<Integer, String>();
+//		클래스 <generic> 인스턴스명 = new 메서드<generic>();
+		// HashMap 이면서 generic이다.
+		// 속도가 빠르다. - 로직순이 아니라 원하는 위치(Hash code)로 바로 이동.
+		// put : 입력(추가)
+		hm1.put(new Integer(100), "멋쟁이");
+		hm1.put(new Integer(300), "공주");
+		hm1.put(new Integer(200), "왕자");
+
+		System.out.println("===============");
+		System.out.println(hm1.get(300));// 키값입력시 밸류값 출력
+		System.out.println(hm1.keySet());// 키값출력
+		System.out.println(hm1.remove(200));// 키값200에 해당하는 해쉬맵 삭제
+		System.out.println(hm1.values());// 밸류값출력
+
+		System.out.println("===============");
+		Collection<String> v1 = hm1.values();
+		System.out.println(v1);
+		
+		System.out.println("===============");
+		Set<Integer> s1 = hm1.keySet();
+		System.out.println(s1);
+
+		System.out.println("===============");
+		for (Integer i : s1) {
+			System.out.println(i + "\t" + hm1.get(i));
+		}
+		System.out.println("===============");
+		Set<Map.Entry<Integer, String>> set3 = hm1.entrySet();
+		//Map.Entery 1) getkey() 2)getvalue
+		for(Map.Entry<Integer, String> k : set3) {
+			System.out.println(k.getKey()+"\t"+k.getValue());
+		}
+		System.out.println("====Hashmap Iterator====");
+		Iterator itr1 = hm1.values().iterator();
+		while (itr1.hasNext()) {
+			System.out.println(itr1.next());
+		}
+	}
+}
---
+++ SWStudy/src/Collections_FrameWork/Queue1.java
@@ -0,0 +1,54 @@
+package Collections_FrameWork;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Iterator interface
+ * 
+ * Queue(fifo:first in first out)
+ *	먼저들어온게 먼저나감
+ * lilo : last in last out
+ * 
+ * collection 인터페이스
+ * ->Queue(큐) interface -> 상속받아 만든 LinkedList클래스
+ * 	LinkedList<String> q2 = new LinkedList<String>();
+ */
+public class Queue1 {
+	public static void main(String[] args) {
+		/*
+		 * Queue q1 = new LinkedList();
+		 */
+		Queue<String> q1 = new LinkedList<String>();
+//	==	LinkedList <String> q1 = new LinkedList<String>();
+
+		q1.offer("바보");
+		q1.offer("온달");
+		q1.offer("평강공주");
+
+		System.out.println(q1.size()); // 3
+		System.out.println(q1); // [바보, 온달, 평강공주]
+
+		Iterator<String> itr1 = q1.iterator();
+		while (itr1.hasNext()) {
+			String name1 = itr1.next();
+			System.out.println(name1);
+
+			System.out.println(itr1.next());
+
+			if (!q1.isEmpty()) {	// q1.이 비어있지 않나?
+				q1.poll();			// 바보 꺼내는 메소드
+			}
+			if (!q1.isEmpty()) {	// q1.이 비어있지 않나?
+				q1.poll();			// 온달 꺼내는 메소드
+			}
+			q1.offer("홍길동");		// 평강공주 뒤에 홍길동을 추가
+			
+			System.out.println(q1.size());
+			System.out.println(q1.peek()); // 처음 값 출력
+			System.out.println(q1);
+		}
+	}
+
+}
