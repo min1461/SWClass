2814e6cdde2009c32f2ab300eeb776f91006f289
min1461 <min1461@naver.com>
2020-11-18 01:14:12 +0900

    fileReader,BufferedReader

---
+++ SWStudy/src/FileS/Stream.java
@@ -0,0 +1,45 @@
+package FileS;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * file
+ * Stream : byte Stream / 문자 Stream
+ * 				input / output Stream
+ * input output Stream이 들어가 있으면 바이트 없으면 문자
+ * 
+ * file을 처리하는 로직(text viewer 의 경우)
+ * open (owner)
+ * read(write)
+ * 화면출력
+ * close(free)
+ */
+public class Stream {
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("C://workspace_SW/SWStudy/aa.txt"); // -> Open
+		/*
+		 * (참고)
+		 * input, output Stream이 보이면 바이트Stream
+		 * reader, writer 가 보이면 문자 Stream
+		 */
+		BufferedReader br = new BufferedReader(fr);
+		/*
+		 * 임시방에 버퍼링 한 후 한줄 처리(효과적인 파일 저장을 위한 방법)
+		 * read는 파일에서 읽어서 to 기억창고(변수)
+		 * write는 키보드 등으로 입력하여 to 파일에
+		 */
+		
+		//BufferedReader br = new BufferedReader(new FileReader("C://workspace_SW/SWStudy/aa.txt"));
+		
+		String s = null;
+		//file read - 화면출력(write) //byte readInt
+		while((s=br.readLine())!= null) {
+			System.out.println((s));
+		}
+		br.close();
+		fr.close();
+
+	}
+}
---
+++ SWStudy/src/FileS/Stream2.java
@@ -0,0 +1,39 @@
+package FileS;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+/*
+ * OutputStreamWriter는 반대형태의 중간역할
+ * 
+ * input, output -> byte 단위
+ * reader,writer -> 문자 단위
+ */
+public class Stream2 {
+	public static void main(String[] args) throws IOException {
+
+		// 키보드 입력을 위해
+//		InputStreamReader isr1 = new InputStreamReader(System.in);
+//		BufferedReader br1 = new BufferedReader(isr1);
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));	// write하기전에 리더를 생성
+//		FileWriter fw = new FileWriter("C://workspace_SW/SWStudy/cc.txt",false);	//	주소값에 해당하는 내용을 write하기위함
+		// false를 써서 기존에 저장되어 있는 내용을 지우기 <-> true는 기존 내용을 보존함
+//		BufferedWriter bw = new BufferedWriter(fw);		// 임시방을 만들어 쓰는 writer
+//		PrintWriter pw = new PrintWriter(bw);	// 줄바꿈이 가능한 writer
+//		PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("C://workspace_SW/SWStudy/cc.txt",false)));// Line 24~26 한줄로 생성 가능
+		
+		System.out.println("저장할 데이터를 입력해주세요(입력끝은 ctrl+z)");
+		String s = null;
+		while((s = br1.readLine()) !=null){
+			pw.println(s);
+		}
+		pw.flush();
+		br1.close();
+		pw.close();
+	}
+}
