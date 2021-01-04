fde41f1d65460cf9d0ad5cbf6289d311758aa629
min1461 <min1461@naver.com>
2021-01-04 21:53:38 +0900

    수업 및 tcp소켓프로그래밍

--- TCP/src/TCP_3/KajaClientGUI.java
+++ TCP/src/TCP_3/KajaClientGUI.java
@@ -19,80 +19,87 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class KajaClientGUI extends JFrame implements Runnable, ActionListener {
-	// console 모드에서 넘어오는 3개 인자를 받아 저장할 준비필드
+	// thread를 위해
+	// console모드에서 넘어오는 3개 인자를 받아 저장할 준비필드
 	DataOutputStream outputStream;
 	DataInputStream inputStream;
 	String nickname;
-
+	// 그래픽디자인 컴포넌트 를 준비
 	JLabel jlabel1 = new JLabel("우리반 채팅~~~");
 	JTextArea jtarea1 = new JTextArea();
 	JTextField jtfield1 = new JTextField();
-	JScrollPane jScrollPane = new JScrollPane(jtarea1);
+	JScrollPane jScrollPane = new JScrollPane(jtarea1);/////////////
 
-//	클라이언트의
-//	new KajaClientGUI(outputstream,inputstream,args[2])
-//	{에서 여기로 찾아옴
-//	GUI의 생성자 처리 
-	public KajaClientGUI(DataOutputStream outputStream, DataInputStream inputStream, String nickname) {
-		this.outputStream = outputStream;
+	// 클라이언트의
+	// new kajaClientGUI(outputStream, inputStream, args[2])
+	// {에서 여기로 찾아옴
+	// gui 만의 생성자처리 시작
+	public KajaClientGUI(DataOutputStream outputStream, DataInputStream inputStream, String nickname) {// 생성자
+		this.outputStream = outputStream;// 필드(속성) 초기치
 		this.inputStream = inputStream;
 		this.nickname = nickname;
 
-		setLayout(new BorderLayout());
-		// NORTH, SOUTH, WEST, EAST, CENTER
+		setLayout(new BorderLayout());/////////////
+		// North, South, West, East, Center
 
 		jlabel1.setFont(new Font("굴림", Font.BOLD, 22));
 		add("North", jlabel1);// "우리반 채팅"
 
-		// chat문자열이 출력되는 곳
-		jtarea1.setBackground(Color.yellow);
-		jtarea1.setForeground(Color.blue);
+		// chat문자열이 출력되는ㄴ 곳
+		jtarea1.setBackground(Color.yellow);// 보색대비
+		jtarea1.setForeground(Color.blue);// 노랑 배경색 위에 파랑색 글씨
 		jtarea1.setFont(new Font("굴림", Font.BOLD, 22));
-		// new Font(글씨체, 스타일, 크기)
 
-		jtarea1.setEditable(false);
+		jtarea1.setEditable(false); // 편집 x
 		add("Center", jScrollPane);
+		// new JScrollPane(jtarea1) // 이미 연결선언되어있음
+		// add("Center", tarea1) //그러므로 이것 대신 위에것 사용;///////////
 
 		// chat 입력
 		jtfield1.setBackground(Color.white);
 		jtfield1.setForeground(Color.BLACK);
 		jtfield1.setFont(new Font("굴림", Font.BOLD, 25));
-		add("South", jtfield1);
-		jtfield1.addActionListener(this);// 이벤트등록
+		add("South", jtfield1);///////////
+		jtfield1.addActionListener(this);// 이벤트 등록//////////////////
 
-		setTitle(nickname+"의 채팅방");
-		setSize(800, 800);
-		setVisible(true);
+		setSize(800, 800);// 채팅창 크기
+		setVisible(true); // 항상 보여줘
 
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				dispose();
-				System.exit(0);
-//				이걸 해주어야 퇴장f
+				System.exit(0);// 이걸 해주어야 퇴장~~이라는 메시지 나옴
+				// 단, 1명접속은 보낼 사람이 없으니 안나오고
+				// 2명이상일때 나옴
 				setVisible(false);
 			}
 		});
 
+		// ==========================================================
 		// 서버로 부터 받아 textarea에 뿌려주는 쓰레드
 		Thread th1 = new Thread(this);
-		th1.start(); // ? 자기자신참조
-	}// 생성자 -end
+		th1.start();
+		// ======================================================
+	}// 생성자-end
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == jtfield1) {
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == jtfield1) {
 			try {
-				outputStream.writeUTF(nickname + "---->" + jtfield1.getText());
-				// 서버 //nickname과 client의 chat을 서버로
+				outputStream.writeUTF(nickname + " --> " + jtfield1.getText());
+				// nickname과 client의 chat을 서버로
 			} catch (IOException ee) {
+				// ee.printStackTrace();
 			}
-			jtfield1.setText("");// (서버로 보내고난후 ) 해당 칸을 클리어
+			jtfield1.setText("");// 서버로 보내고 해당 칸을 클리어
 		}
-	} // actionPerformed-end
 
-	// chat 올때마다 beep음 울리게
+	}// actionPerformed - end
+
 	Toolkit tk1 = Toolkit.getDefaultToolkit();
+	// chat올때마다 beep음 울리게 하려고
 
+	@Override
 	public void run() { // for 받는 thread
 		try {
 			while (true) {
@@ -101,16 +108,19 @@ public class KajaClientGUI extends JFrame implements Runnable, ActionListener {
 					jtarea1.append("\n" + "종료");
 					return;
 				}
-				jtarea1.append("\n" + strServer1); // 서버에서 온것을 textarea에 추가
+				jtarea1.append("\n" + strServer1);// 서버에서온것 textarea에추가
 
-				// 이것을 해야 스크롤 바가 생긴후 맨 마지막 내용이 잘보임
+				// ---------이것해야 스크롤바가 생긴후 맨 마지막 내용이 잘 보임 -----
 				int kkeut = jtarea1.getText().length();
 				jtarea1.setCaretPosition(kkeut);// 커서를 맨뒤로
-				// jtarea1.setCaretPosition(0);//커서를 맨처음으로
-				tk1.beep(); // chat이 올때마다 beep음
+				// jtarea1.setCarePosition(0);//커서를 맨처음에
+				tk1.beep();//////////// chat이 올때마다 beep음 (띠링~~~~) // 이어폰꽂고들어보자
+				// -------------------------------------------
 			}
 		} catch (Exception eee) {
 			jtarea1.append("\n" + eee);
 		}
-	}// run-end
-}// class-end
+
+	}
+
+}
--- TCP/src/TCP_3/TcpMulServer.java
+++ TCP/src/TCP_3/TcpMulServer.java
@@ -8,69 +8,74 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 class ServerClass {
-	ArrayList<ThreadServerClass> threadList = new ArrayList<ThreadServerClass>();
-	Socket socket;
-	DataOutputStream DataOutputStream;
 
-	public ServerClass(int portno) throws IOException {
+	ArrayList<ThreadServerClass> threadList = new ArrayList<ThreadServerClass>();
+
+	Socket socket;
+	DataOutputStream outputStream;
+
+	public ServerClass(int portno) throws IOException {// 생성자
+		// public ServerClass() throws IOException {
+
 		Socket s1 = null;
 
-//		ServerSocket ss1 = new ServerSocket(6788);
+		// ServerSocket ss1 = new ServerSocket(6788);
 		ServerSocket ss1 = new ServerSocket(portno);
-		System.out.println("서버 가동...");
+		System.out.println("서버가동........");////////////////////
 
 		while (true) {
 			s1 = ss1.accept();
-			System.out.println("접속주소 : " + s1.getInetAddress() + " , 접속포트 : " + s1.getPort());
+			System.out.println("접속주소: " + s1.getInetAddress() + " , 접속포트: " + s1.getPort());
 			ThreadServerClass tServer1 = new ThreadServerClass(s1);
-			tServer1.start();
-
+			tServer1.start();////////
+			// 한명이 접속하면 ThreadServerClass 쓰레드에 올려놓음
 			threadList.add(tServer1);// 컬렉션에 add
-			System.out.println("접속자수 : " + threadList.size() + " 명");
-		} // while문의 끝
+			System.out.println("접속자 수: " + threadList.size());
+		} // while -end
 	}
 
 	// 접속중인 쓰레드에게 chat 내용 보냄
 	public void sendChat(String chat) throws IOException {
-		for (int i = 0; i < threadList.size(); i++) {
-			threadList.get(i).OutputStream.writeUTF(chat);
-			// 처음에 nickname이 채팅을하는 모든사람에게 전송(서버가하는일)
-		}
+		for (int i = 0; i < threadList.size(); i++)
+			threadList.get(i).outputStream.writeUTF(chat);
+		// 처음에 nickname이 채팅관련모든 사람에게 전송
 	}
-	
-	class ThreadServerClass extends Thread {
+
+	class ThreadServerClass extends Thread { // 한명 접속마다 처리한 쓰레드 클래스
 		Socket socket1;
 		DataInputStream inputStream;
-		DataOutputStream OutputStream;
+		DataOutputStream outputStream;
 
 		public ThreadServerClass(Socket s1) throws IOException {
 			socket1 = s1;
 			inputStream = new DataInputStream(s1.getInputStream());
-			OutputStream = new DataOutputStream(s1.getOutputStream());
+
+			outputStream = new DataOutputStream(s1.getOutputStream());
 		}
 
-		public void run() {
+		@Override
+		public void run() { // remember !!!!!! 한사람 서버로 접속한 경우임
 			String nickname = "";
 			try {
 				if (inputStream != null) {
-					nickname = inputStream.readUTF();
-					sendChat(nickname + " 님 입장~~");
+					nickname = inputStream.readUTF();// 초록왕자,은빛공주
+					sendChat(nickname + "님 입장~~~~~ (^^) (^^) (^^)");
 				}
 				while (inputStream != null) {
-//				System.out.println(inputStream.readUTF());
-					sendChat(inputStream.readUTF());
+					// System.out.println(inputStream.readUTF());
+					sendChat(inputStream.readUTF()); // 방가방가 ~~~
 					// 클라이언트가 보낸 채팅 내용을 접속한 모두에게 보냄
 				}
-			} catch (IOException e) {
+				// 정상채팅의 경우는 계속 while 문 안에서 반복 loop
+			} catch (IOException e) { // 여기로 왔단 얘기는 에러가 발생한 것 //나가버린 경우
 				e.printStackTrace();
 			} finally {
 				// 나간 쓰레드의 인덱스 찾기
 				for (int i = 0; i < threadList.size(); i++) {
-					if (socket1.equals(threadList.get(i).socket1)) {
-						// 소켓의 주인을 찾아서~
-						threadList.remove(i);// 소켓의 주인은 ~nickname
+					if (socket1.equals(threadList.get(i).socket1)) {// 이 소켓이 누구거???????
+						threadList.remove(i);// 찾았다 이도령 소켓
 						try {
-							sendChat(nickname + " 님 퇴장~~~~");
+							sendChat(nickname + "님 퇴장~~~~ (ㅠ.ㅠ) (ㅠ.ㅠ) (ㅠ.ㅠ)");
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
@@ -78,17 +83,20 @@ class ServerClass {
 				}
 				System.out.println("접속자 수 : " + threadList.size() + " 명");
 			} // finally-end
-		}// run-end
-	}// ThreadServerClass-end
-}// Server Class-end
+		}// run -end
+	}// threadServerClass-end
+
+}// ServerClass-end
 
 public class TcpMulServer {
+
 	public static void main(String[] args) throws IOException {
 		if (args.length != 1) {
-			System.out.println("사용법 : 서버실행은 \'java 패키지명 .파일 포트번호\' 형식으로 입력");
-		}
-//		new ServerClass();
-		new ServerClass(Integer.parseInt(args[0]));
 
+			System.out.println("사용법 : 서버실행은 \'java 패키지명.파일명 포트번호\' 형식으로 입력");
+		}
+		// new ServerClass();///////////
+		new ServerClass(Integer.parseInt(args[0]));/////////////
 	}
+
 }
